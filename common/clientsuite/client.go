6764f21fc691b8723b7d4a843c70810a25d46a40
zzb <1072396779@qq.com>
2025-02-10 19:42:41 +0800

    refactor: 优化客户端和服务端的配置逻辑，调整 Consul 注册器和解析器的创建顺序

--- common/clientsuite/client.go
+++ common/clientsuite/client.go
@@ -17,29 +17,29 @@ type CommonGrpcClientSuite struct {
 
 // Options 实现Suite接口的Options方法
 func (s CommonGrpcClientSuite) Options() []client.Option {
+	// 基础客户端配置
+	opts := []client.Option{
+		// 设置 Client 侧的 Service 信息，用于服务发现
+		client.WithClientBasicInfo(&rpcinfo.EndpointBasicInfo{
+			ServiceName: s.CurrentServiceName,
+		}),
+		// 基础客户端配置
+		client.WithMetaHandler(transmeta.ClientHTTP2Handler),
+		// 使用 GRPC 作为传输协议
+		client.WithTransportProtocol(transport.GRPC),
+		// 设置链路追踪
+		client.WithSuite(tracing.NewClientSuite()),
+	}
+
 	// 创建consul解析器
 	r, err := consul.NewConsulResolver(s.RegistryAddr)
 	if err != nil {
 		panic(err)
 	}
 
-	// 基础客户端配置
-	opts := []client.Option{
+	opts = append(opts,
 		// 指定一个 Resolver 进行服务发现
 		client.WithResolver(r),
-		// 基础客户端配置
-		client.WithMetaHandler(transmeta.ClientHTTP2Handler),
-		// 使用 GRPC 作为传输协议
-		client.WithTransportProtocol(transport.GRPC),
-	}
-
-	opts = append(opts,
-		// 设置 Client 侧的 Service 信息，用于服务发现
-		client.WithClientBasicInfo(&rpcinfo.EndpointBasicInfo{
-			ServiceName: s.CurrentServiceName,
-		}),
-		// 设置链路追踪
-		client.WithSuite(tracing.NewClientSuite()),
 	)
 
 	return opts
